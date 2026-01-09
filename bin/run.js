856fe4b12eb5b511086f7ae229616afd4d77a232
Taesu <bytaesu@gmail.com>
2026-01-09 21:11:42 +0900

    binary

--- bin/run.js
+++ bin/run.js
@@ -1,12 +1,21 @@
 #!/usr/bin/env node
-const { spawn } = require("node:child_process");
+const { spawn, spawnSync } = require("node:child_process");
+const fs = require("node:fs");
 const path = require("node:path");
 
 const BIN_NAME = process.platform === "win32" ? "create-github-app.exe" : "create-github-app";
 const BIN_PATH = path.join(__dirname, BIN_NAME);
 
-const child = spawn(BIN_PATH, process.argv.slice(2), { stdio: "inherit" });
+function run() {
+	const child = spawn(BIN_PATH, process.argv.slice(2), { stdio: "inherit" });
+	child.on("close", (code) => process.exit(code));
+}
 
-child.on("close", (code) => {
-  process.exit(code);
-});
+if (!fs.existsSync(BIN_PATH)) {
+	const install = spawnSync("node", [path.join(__dirname, "install.js")], { stdio: "inherit" });
+	if (install.status !== 0) {
+		process.exit(1);
+	}
+}
+
+run();
